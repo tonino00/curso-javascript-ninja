729cd25b01e20183acd4fcfad8af7e8a616cb217
tonino00 <antoninocgama@hotmail.com>
2020-04-15 13:52:07 -0300

    respostas exercicio 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,36 +2,41 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
-
+var myArray = [5, 'Eu', null, { a: 2 }, function () { }];
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
-
+function myFunction(arr) {
+  return arr;
+}
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myFunction(myArray)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
 segundo, um número. A função deve retornar o valor de um índice do array que foi passado
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
-*/
+*/ function myFunction2(arr, index) {
+  return arr[index];
+}
 // ?
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var myArray2 = ['Ninja', 2.3, false, [1, 2, 3, 'ACG'], { b: 2 }];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
-array criado.
-*/
-// ?
+array criado.*/
+console.log(myFunction2(myArray2, 0));
+console.log(myFunction2(myArray2, 1));
+console.log(myFunction2(myArray2, 2));
+console.log(myFunction2(myArray2, 3));
+console.log(myFunction2(myArray2, 4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +52,42 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+function book(bookName) {
+  const allBooks = {
+    'Segredos do JavaScript': { quantidadePaginas: 400, autor: ' Luiz Amarante', editora: 'Casa do código' },
+    'Introdução ao HTML5': { quantidadePaginas: 300, autor: ' Ana Luiza ', editora: 'Casa do código' },
+    'Smashing CSS': { quantidadePaginas: 200, autor: ' Charles Edmound ', editora: 'Casa do código' }
+  }
+
+  return !bookName ? allBooks : allBooks[bookName];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
-
+console.log(book());
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+const bookName = "Introdução ao HTML5";
+console.log('O livro ' + bookName + 'tem' + book(bookName).quantidadePaginas + 'páginas!')
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
-"O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
+'O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+const bookName = "Introdução ao HTML5";
+console.log('O autor do livro' + bookName + 'é' + book(bookName).autor + '.')
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+
+const bookName = "Introdução ao HTML5";
+console.log('O autor do livro' + bookName + 'foi publicado pela editora ' + book(bookName).editora + '.')
